11e3c68b1084049f7d2f8737d2f948b1b7d1060c
Aseeeem-kc <ashimkc7297@gmail.com>
2025-09-28 21:09:53 +0545

    udpate: item models

--- upstart/item/models.py
+++ upstart/item/models.py
@@ -18,7 +18,7 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank = True, null = True)
     price = models.FloatField()
-    image = models.ImageField(upload_to = 'item_images', blank = True, null = True)
+    image = models.ImageField(upload_to = 'item_images', blank = False, null = False, default='item_images/default.png')
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL,related_name = 'items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)    
